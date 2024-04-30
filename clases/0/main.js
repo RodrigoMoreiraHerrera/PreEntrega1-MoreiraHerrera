6f6aa83f722432814487fbe4705075721655da35
RodrigoMoreiraHerrera <140863682+RodrigoMoreiraHerrera@users.noreply.github.com>
2024-04-29 23:31:57 -0300

    Delete clases directory

--- clases/0/main.js
+++
@@ -1,8 +0,0 @@
-
-
-let numero1 = parseInt(prompt("Ingresa un numero"))
-let numero2 = parseInt(prompt("Ingresa otro numero"))
-
-if (numero1 %2 === 0  && numero2 %2 === 0) {
-    alert("son pares")
-} 
